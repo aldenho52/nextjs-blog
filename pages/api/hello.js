644cdc568ed81dbe35f9671af96cdfd0a89c77c8
alden.ho <alden.ho@advinow.com>
2021-10-10 16:08:15 -0700

    remove additional default export

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,3 @@
 export default function handler(req, res) {
   res.status(200).json({ text: "Hello" });
 }
-
-export default function saveForm(req, res) {
-  const email = req.body.email;
-  // Then save email to your database, etc...
-}
